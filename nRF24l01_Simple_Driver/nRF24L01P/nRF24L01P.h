b1aebbc977cc5b563440a00e1cb5e486cb2f8422
Abdallah Ali <abdallah.ali.abdallah.elmasry@gmail.com>
2016-02-02 19:09:24 +0200

    update the nRF Library

---
+++ nRF24l01_Simple_Driver/nRF24L01P/nRF24L01P.h
@@ -0,0 +1,28 @@
+
+#define ADR_WIDTH 5
+
+#define RX_PLOAD_WIDTH 20
+#define TX_PLOAD_WIDTH 20
+#define Buffer_Size 32
+
+//Define RF power value
+#define P0dBm 0
+#define Pm6dBm 1
+#define Pm12dBm 2
+#define Pm18dBm 3
+
+//#define RF rate
+#define R2mbps 0
+#define R1mbps 1
+#define R250kbps 3
+
+
+unsigned char nRF24L01_Config(unsigned char freq,unsigned char power,unsigned char rate);
+void RX_Mode(void);
+void TX_Mode(void);
+void nRF24L01_TxPacket(unsigned char * tx_buf);
+unsigned char nRF24L01_RxPacket(unsigned char* rx_buf);
+void nRF24L01_HW_Init(void);
+void nRF24L01_Set_TX_Address(unsigned char A,unsigned char B,unsigned char C,unsigned char D,unsigned char E);
+void nRF24L01_Set_RX_Address(unsigned char A,unsigned char B,unsigned char C,unsigned char D,unsigned char E);
+
